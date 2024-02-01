3041ed92760daa7281c92be3f1c05fc70f926940
Joepolymath <joshuaajagbe96@gmail.com>
2024-02-01 02:20:42 +0100

    jest setup

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+// jest.config.js
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/*.test.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/packages/$1',
+  },
+};
---
+++ packages/usersService/__tests__/try.test.ts
@@ -0,0 +1,5 @@
+// math.test.ts
+test('1 + 1 equals 2', () => {
+  const result = 1 + 1;
+  expect(result).toBe(2);
+});
