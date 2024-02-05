efcb59f262becf26918a2bc852bcd554b112fafe
Joepolymath <joshuaajagbe96@gmail.com>
2024-02-05 19:15:10 +0100

    rabbitmq connection function

---
+++ packages/usersService/setup/rabbitmq.ts
@@ -0,0 +1,23 @@
+import { connect, Connection, Channel } from 'amqplib';
+
+interface RabbitMQConfig {
+  url: string;
+}
+
+export async function createRabbitMQConnection(
+  config: RabbitMQConfig
+): Promise<{ connection: Connection; channel: Channel }> {
+  try {
+    const connection = await connect(config.url);
+
+    const channel = await connection.createChannel();
+
+    console.log('Connected to RabbitMQ successfully.');
+
+    return { connection, channel };
+  } catch (error: any) {
+    if (error.message)
+      console.error('Error connecting to RabbitMQ:', error.message);
+    throw error;
+  }
+}
